c073c36b91113e1a617ac078d98a89cf1889b9a7
Vishnu Nair <vishnu4nair@gmail.com>
2016-06-30 19:55:17 -0400

    FINGERPRINTER - Adding initial version of fingerprinting app with map now showing on main screen.

---
+++ app/src/main/java/ble/localization/fingerprinter/MainActivity.java
@@ -0,0 +1,22 @@
+package ble.localization.fingerprinter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.davemorrissey.labs.subscaleview.ImageSource;
+import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SubsamplingScaleImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (SubsamplingScaleImageView)findViewById(R.id.imageView);
+        imageView.setImage(ImageSource.resource(R.drawable.home_floor_plan));
+
+    }
+}
