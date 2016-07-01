46fbf99bce0da67276d9a2d1cb77c3011eac1bff
Vishnu Nair <vishnu4nair@gmail.com>
2016-06-30 21:00:16 -0400

    Adding base beacon discovery code.

--- app/src/main/java/ble/localization/fingerprinter/MainActivity.java
+++ app/src/main/java/ble/localization/fingerprinter/MainActivity.java
@@ -6,10 +6,23 @@ import android.os.Bundle;
 
 import com.davemorrissey.labs.subscaleview.ImageSource;
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
+import com.estimote.sdk.Beacon;
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Region;
+import com.estimote.sdk.SystemRequirementsChecker;
+
+import java.util.List;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
     private SubsamplingScaleImageView imageView;
+
+    private BeaconManager beaconManager;
+    private Region region;
+    // When ready to range, "beaconManager.startRanging(region);"
+    // When stopping, "beaconManager.stopRanging(region);"
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +35,21 @@ public class MainActivity extends AppCompatActivity {
 
         imageView = (SubsamplingScaleImageView)findViewById(R.id.imageView);
         imageView.setImage(ImageSource.resource(R.drawable.home_floor_plan));
-        
+
         loading_dialog.dismiss();
+
+        SystemRequirementsChecker.checkWithDefaultDialogs(this);
+
+        beaconManager = new BeaconManager(this);
+        region = new Region("ranged region", UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), null, null);
+
+        beaconManager.setRangingListener(new BeaconManager.RangingListener() {
+            @Override
+            public void onBeaconsDiscovered(Region region, List<Beacon> list) {
+                // Beacon discovery code goes here.
+            }
+        });
+
 
     }
 }
