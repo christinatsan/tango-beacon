3a353fad5c1bca8fef633e72bf350d6792595c3a
Vishnu Nair <vishnu4nair@gmail.com>
2016-06-30 22:01:17 -0400

    Added some statistics-related stuff.

--- app/src/main/java/ble/localization/fingerprinter/MainActivity.java
+++ app/src/main/java/ble/localization/fingerprinter/MainActivity.java
@@ -6,15 +6,28 @@ import android.os.Bundle;
 
 import com.davemorrissey.labs.subscaleview.ImageSource;
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
+
 import com.estimote.sdk.Beacon;
 import com.estimote.sdk.BeaconManager;
 import com.estimote.sdk.Region;
 import com.estimote.sdk.SystemRequirementsChecker;
 
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
+import cern.colt.list.DoubleArrayList;
+import cern.jet.stat.Descriptive;
+
+// To jsonify: https://developer.android.com/reference/org/json/JSONObject.html
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PERCENT_CUTOFF = 15;
 
     private SubsamplingScaleImageView imageView;
 
@@ -23,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
     // When ready to range, "beaconManager.startRanging(region);"
     // When stopping, "beaconManager.stopRanging(region);"
 
+    Map<Integer, ArrayList<Integer>> beacon_rssi_values = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,5 +65,17 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    public static double trimmean(final ArrayList<Double> arr_list, final int percent) {
+        Double[] arr2 = arr_list.toArray(new Double[arr_list.size()]);
+        double[] arr = ArrayUtils.toPrimitive(arr2);
+
+        final int n = arr.length;
+        final int k = (int)Math.round(n * (percent / 100.0) / 2.0);
+        final DoubleArrayList list = new DoubleArrayList(arr);
+        list.sort();
+
+        return Descriptive.trimmedMean(list, Descriptive.mean(list), k, k);
     }
 }
