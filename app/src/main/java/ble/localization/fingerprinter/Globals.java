c1f484413d196d1b577a5e3e7726888728d244d1
Vishnu Nair <vishnu4nair@gmail.com>
2016-09-29 21:56:44 -0400

    Removed some unnecessary access modifiers.

--- app/src/main/java/ble/localization/fingerprinter/Globals.java
+++ app/src/main/java/ble/localization/fingerprinter/Globals.java
@@ -14,30 +14,30 @@ import java.util.UUID;
 /**
  * Created by vishnunair on 7/6/16.
  */
-public class Globals {
+class Globals {
 
-    protected static final String PHASE_CHANGE_BROADCAST_PAYLOAD_KEY = "TARGET_PHASE";
-    protected static final String SERVER_BASE_URL = "http://192.168.0.10:8000/api";
+    static final String PHASE_CHANGE_BROADCAST_PAYLOAD_KEY = "TARGET_PHASE";
+    static final String SERVER_BASE_URL = "http://192.168.0.10:8000/api";
 
-    protected static final UUID beaconRegionUUID = UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D");
-    protected static final String beaconRegionName = "ranged region";
-    protected static final Region region = new Region(beaconRegionName, beaconRegionUUID, null, null);
+    private static final UUID beaconRegionUUID = UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D");
+    private static final String beaconRegionName = "ranged region";
+    static final Region region = new Region(beaconRegionName, beaconRegionUUID, null, null);
 
     static final String[] floor_names = {"C-Level", "Ground Floor"};  // Manually set
     static final int floor_start_index = 1; // Manually set
 
-    protected static void disconnectBeaconManager(BeaconManager mBeaconManager, boolean[] isEstimoteRangingServiceReady) {
+    static void disconnectBeaconManager(BeaconManager mBeaconManager, boolean[] isEstimoteRangingServiceReady) {
         mBeaconManager.disconnect();
         isEstimoteRangingServiceReady[0] = false;
     }
 
-    protected static void showSnackbar(View view, String snackbarText) {
+    static void showSnackbar(View view, String snackbarText) {
         Snackbar
                 .make(view, snackbarText, Snackbar.LENGTH_SHORT)
                 .show();
     }
 
-    protected static void showDialogWithOKButton(Context context, String title, String message) {
+    static void showDialogWithOKButton(Context context, String title, String message) {
         new AlertDialog.Builder(context)
                 .setTitle(title)
                 .setMessage(message)
